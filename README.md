c1f4cc3f83ab63ef80cf7db387ed9bb00b07609b
Andy Church <ilioscio@gmail.com>
2021-06-21 22:20:56 -0600

    +prettier added to project,+instances of var changed to let in api.js

--- README.md
+++ README.md
@@ -1,323 +1,312 @@
 # About
+
 REST proxy for zAuction
 
 # Includes
-* [dotenv](https://www.npmjs.com/package/dotenv)
-  * Dotenv is a zero-dependency module that loads environment variables from a `.env` file into `process.env`
-* [eslint](https://www.npmjs.com/package/eslint)
-  * ESLint is a tool for identifying and reporting on patterns found in ECMAScript/JavaScript code.
-* [helmet](https://www.npmjs.com/package/helmet)
-  * Helmet helps you secure your Express apps by setting various HTTP headers. It's not a silver bullet, but it can help!
-* [mocha](https://www.npmjs.com/package/mocha)
-  * ☕️ Simple, flexible, fun JavaScript test framework for Node.js & The Browser ☕️
-* [morgan](https://www.npmjs.com/package/morgan)
-  * HTTP request logger middleware for node.js
-* [nodemon](https://www.npmjs.com/package/nodemon)
-  * nodemon is a tool that helps develop node.js based applications by automatically restarting the node application when file changes in the directory are detected.
-* [supertest](https://www.npmjs.com/package/supertest)
-  * HTTP assertions made easy via superagent.
+
+- [dotenv](https://www.npmjs.com/package/dotenv)
+  - Dotenv is a zero-dependency module that loads environment variables from a `.env` file into `process.env`
+- [eslint](https://www.npmjs.com/package/eslint)
+  - ESLint is a tool for identifying and reporting on patterns found in ECMAScript/JavaScript code.
+- [helmet](https://www.npmjs.com/package/helmet)
+  - Helmet helps you secure your Express apps by setting various HTTP headers. It's not a silver bullet, but it can help!
+- [mocha](https://www.npmjs.com/package/mocha)
+  - ☕️ Simple, flexible, fun JavaScript test framework for Node.js & The Browser ☕️
+- [morgan](https://www.npmjs.com/package/morgan)
+  - HTTP request logger middleware for node.js
+- [nodemon](https://www.npmjs.com/package/nodemon)
+  - nodemon is a tool that helps develop node.js based applications by automatically restarting the node application when file changes in the directory are detected.
+- [supertest](https://www.npmjs.com/package/supertest)
+  - HTTP assertions made easy via superagent.
 
 ## Setup
+
 ```
 npm install
 ```
 
 ## Development
+
 ```
 npm run dev
 ```
 
 ## Test
+
 ```
 npm run test
 ```
 
 ## Lint
+
 ```
 npm run lint
 ```
 
 # API Documentation
-**Create a new auction**
-----
-  Creates a new auction.
 
-* **URL**
+## **Create a new auction**
+
+Creates a new auction.
+
+- **URL**
 
   /api/auction
 
-* **Method:**
+- **Method:**
 
   `POST`
 
-* **URL Params**
+- **URL Params**
 
   None
-   
-* **Data Params**
 
-   **Required:**
- 
-   `account=[string]`  
+- **Data Params**
+
+  **Required:**
+
+  `account=[string]`  
    `tokenID=[string]`  
    `contractAddress=[string]`  
    `startTime=[number]`  
    `endTime=[number]`  
    `minBid=[number]`  
-   `auctionType=[number]`  
+   `auctionType=[number]`
 
-* **Success Response:**
+- **Success Response:**
 
-  * **Code:** 200 Ok<br />
+  - **Code:** 200 Ok<br />
     **Content:**  
     `{ "message", "Ok"}`
- 
-* **Error Response:**
 
-  * **Code:** 400 Bad Request<br />
+- **Error Response:**
+
+  - **Code:** 400 Bad Request<br />
     **Content:**  
     `{ "message", "{param} not found"}`
 
-* **Sample Call:**
+- **Sample Call:**
 
   ```javascript
-    $.ajax({
-      url: "/api/auction",
-      dataType: "json",
-      type : "POST",
-      data : {
-        account : "0xC83507FB1cc907d67BbA67D620c591275af18A62",
-        tokenID : "55525",
-        contractAddress : "0x7A41Ff1b4013D8A0c04863020Ee6F068A6C3b22d",
-        startTime : 0,
-        endTime : 0,
-        minBid : 0.1 
-      },
-      success : function(r) {
-        console.log(r);
-      }
-    });
+  $.ajax({
+    url: "/api/auction",
+    dataType: "json",
+    type: "POST",
+    data: {
+      account: "0xC83507FB1cc907d67BbA67D620c591275af18A62",
+      tokenID: "55525",
+      contractAddress: "0x7A41Ff1b4013D8A0c04863020Ee6F068A6C3b22d",
+      startTime: 0,
+      endTime: 0,
+      minBid: 0.1,
+    },
+    success: function (r) {
+      console.log(r);
+    },
+  });
   ```
-----
-**Get details on an auction**
-----
-  Returns details about an auction.
 
-* **URL**
+---
+
+## **Get details on an auction**
+
+Returns details about an auction.
+
+- **URL**
 
   /api/auctions/{auctionID}
 
-* **Method:**
+- **Method:**
 
   `GET`
 
-*  **URL Params**
+- **URL Params**
 
-   **Required:**
- 
-   `auctionID=[string]`
+  **Required:**
 
-* **Data Params**
+  `auctionID=[string]`
+
+- **Data Params**
 
   None
 
-* **Success Response:**
+- **Success Response:**
 
-  * **Code:** 200 Ok<br />
-    **Content:**   
-    `{
-    "account": "0x7A41Ff1b4013D8A0c04863020Ee6F068A6C3b22d",
-    "auctionType": "0",
-    "bidMsg": "0x4f03254c5da5b3acdc970cfd6f68e8d054beee4bcc291fd005061914bf14335c6ba72c9a5ab80d8ce27092edf6a9d911046dfdb4f24697447e62a524f962873c1c",
-    "contractAddress": "0x6EbeAf8e8E946F0716E6533A6f2cefc83f60e8Ab",
-    "currentBid": 0.1,
-    "currentBidder": "0xC83507FB1cc907d67BbA67D620c591275af18A62",
-    "endTime": "0",
-    "minBid": "0.1",
-    "startTime": "0",
-    "tokenID": "376718"
-    }`
- 
-* **Error Response:**
+  - **Code:** 200 Ok<br />
+    **Content:**  
+    `{ "account": "0x7A41Ff1b4013D8A0c04863020Ee6F068A6C3b22d", "auctionType": "0", "bidMsg": "0x4f03254c5da5b3acdc970cfd6f68e8d054beee4bcc291fd005061914bf14335c6ba72c9a5ab80d8ce27092edf6a9d911046dfdb4f24697447e62a524f962873c1c", "contractAddress": "0x6EbeAf8e8E946F0716E6533A6f2cefc83f60e8Ab", "currentBid": 0.1, "currentBidder": "0xC83507FB1cc907d67BbA67D620c591275af18A62", "endTime": "0", "minBid": "0.1", "startTime": "0", "tokenID": "376718" }`
 
-  * **Code:** 500 Internal Server Error <br />
+- **Error Response:**
+
+  - **Code:** 500 Internal Server Error <br />
     **Content:**  
     `{ "message": "The specified key does not exist." }`
 
-* **Sample Call:**
+- **Sample Call:**
 
   ```javascript
-    $.ajax({
-      url: "/api/auctions/16197345900x7A41Ff1b4013D8A0c04863020Ee6F068A6C3b22d",
-      dataType: "json",
-      type : "GET",
-      success : function(r) {
-        console.log(r);
-      }
-    });
+  $.ajax({
+    url: "/api/auctions/16197345900x7A41Ff1b4013D8A0c04863020Ee6F068A6C3b22d",
+    dataType: "json",
+    type: "GET",
+    success: function (r) {
+      console.log(r);
+    },
+  });
   ```
-----
-**Bid on an auction**
-----
-  Creates a bid for a given auction.
 
-* **URL**
+---
+
+## **Bid on an auction**
+
+Creates a bid for a given auction.
+
+- **URL**
 
   /api/auctions/{auctionID}/bid
 
-* **Method:**
+- **Method:**
 
   `POST`
 
-*  **URL Params**
+- **URL Params**
 
-   **Required:**
- 
-   `auctionID=[string]`
-   
-* **Data Params**
+  **Required:**
 
-   **Required:**
- 
-   `account=[string]`  
+  `auctionID=[string]`
+
+- **Data Params**
+
+  **Required:**
+
+  `account=[string]`  
    `bidAmt=[number]`  
-   `bidMsg=[string]`  
+   `bidMsg=[string]`
 
-* **Success Response:**
+- **Success Response:**
 
-  * **Code:** 200 Ok<br />
+  - **Code:** 200 Ok<br />
     **Content:**  
     `{ "message", "Ok"}`
- 
-* **Error Response:**
 
-  * **Code:** 400 Bad Request<br />
+- **Error Response:**
+
+  - **Code:** 400 Bad Request<br />
     **Content:**  
     `{ "message", "{param} not found"}`
 
-* **Sample Call:**
+- **Sample Call:**
 
   ```javascript
-    $.ajax({
-      url: "/api/auctions/16209571250x7A41Ff1b4013D8A0c04863020Ee6F068A6C3b22d/bid",
-      dataType: "json",
-      type : "POST",
-      data : {
-        account : "0x722e8bC37781B758E215c2F1180d9f437Fe91736",
-        bidAmt : 0.3,
-        bidMsg : "Signed Message"
-      },
-      success : function(r) {
-        console.log(r);
-      }
-    });
+  $.ajax({
+    url: "/api/auctions/16209571250x7A41Ff1b4013D8A0c04863020Ee6F068A6C3b22d/bid",
+    dataType: "json",
+    type: "POST",
+    data: {
+      account: "0x722e8bC37781B758E215c2F1180d9f437Fe91736",
+      bidAmt: 0.3,
+      bidMsg: "Signed Message",
+    },
+    success: function (r) {
+      console.log(r);
+    },
+  });
   ```
-----
-**Get the current bids for an auction**
-----
-  Returns the current bids for a given auction.
 
-* **URL**
+---
+
+## **Get the current bids for an auction**
+
+Returns the current bids for a given auction.
+
+- **URL**
 
   /api/auctions/{auctionID}/bids
 
-* **Method:**
+- **Method:**
 
   `GET`
 
-* **URL Params**
+- **URL Params**
 
-   **Required:**
- 
-   `auctionID=[string]`
+  **Required:**
 
-* **Data Params**
+  `auctionID=[string]`
+
+- **Data Params**
 
   None
 
-* **Success Response:**
+- **Success Response:**
 
-  * **Code:** 200 Ok<br />
-    **Content:**   
-    `
-    {
-    "currentBid": 0.1,
-    "currentBidder": "0xC83507FB1cc907d67BbA67D620c591275af18A62"
-    }
-    `
- 
-* **Error Response:**
+  - **Code:** 200 Ok<br />
+    **Content:**  
+    `{ "currentBid": 0.1, "currentBidder": "0xC83507FB1cc907d67BbA67D620c591275af18A62" }`
 
-  * **Code:** 500 Internal Server Error <br />
+- **Error Response:**
+
+  - **Code:** 500 Internal Server Error <br />
     **Content:**  
     `{ "message": "The specified key does not exist." }`
 
-* **Sample Call:**
+- **Sample Call:**
 
   ```javascript
-    $.ajax({
-      url: "/api/auctions/16197345900x7A41Ff1b4013D8A0c04863020Ee6F068A6C3b22d/bids",
-      dataType: "json",
-      type : "GET",
-      success : function(r) {
-        console.log(r);
-      }
-    });
+  $.ajax({
+    url: "/api/auctions/16197345900x7A41Ff1b4013D8A0c04863020Ee6F068A6C3b22d/bids",
+    dataType: "json",
+    type: "GET",
+    success: function (r) {
+      console.log(r);
+    },
+  });
   ```
-  ----
-**Get a list of all auctions**
-----
-  Returns a list of all auction IDs (referred to as keys).
 
-* **URL**
+  ***
+
+  **Get a list of all auctions**
+
+---
+
+Returns a list of all auction IDs (referred to as keys).
+
+- **URL**
 
   /api/auctions
 
-* **Method:**
+- **Method:**
 
   `GET`
 
-* **URL Params**
+- **URL Params**
 
   None
 
-* **Data Params**
+- **Data Params**
 
   None
 
-* **Success Response:**
+- **Success Response:**
 
-  * **Code:** 200 Ok<br />
-    **Content:**   
-    `
-      [
-          {
-              "key": "16197345900x7A41Ff1b4013D8A0c04863020Ee6F068A6C3b22d"
-          },
-          {
-              "key": "16210130910xC83507FB1cc907d67BbA67D620c591275af18A62"
-          },
-          {
-              "key": "16210194470x9C7879689054685E31ee4097f74ac5091AD09De0"
-          }
-      ]
-    `
- 
-* **Error Response:**
+  - **Code:** 200 Ok<br />
+    **Content:**  
+    ` [ { "key": "16197345900x7A41Ff1b4013D8A0c04863020Ee6F068A6C3b22d" }, { "key": "16210130910xC83507FB1cc907d67BbA67D620c591275af18A62" }, { "key": "16210194470x9C7879689054685E31ee4097f74ac5091AD09De0" } ]`
+
+- **Error Response:**
 
   None
 
-* **Sample Call:**
+- **Sample Call:**
 
   ```javascript
-    $.ajax({
-      url: "/api/auctions",
-      dataType: "json",
-      type : "GET",
-      success : function(r) {
-        console.log(r);
-      }
-    });
+  $.ajax({
+    url: "/api/auctions",
+    dataType: "json",
+    type: "GET",
+    success: function (r) {
+      console.log(r);
+    },
+  });
   ```
-----
 
+---
--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const fleek = require('@fleekhq/fleek-storage-js');
-const rateLimit = require('express-rate-limit');
-const ethers = require('ethers');
-const abi = require('./abi.json');
+const express = require("express");
+const fleek = require("@fleekhq/fleek-storage-js");
+const rateLimit = require("express-rate-limit");
+const ethers = require("ethers");
+const abi = require("./abi.json");
 
 const router = express.Router();
 
@@ -24,29 +24,37 @@ const contract = new ethers.Contract(contractAddress, abi, signer);
 const secrets = {
   apiKey: process.env.FLEEK_API_KEY,
   apiSecret: process.env.FLEEK_API_SECRET,
-  infuraSecret: process.env.INFURA_API_SECRET
-}
+  infuraSecret: process.env.INFURA_API_SECRET,
+};
 
 // Function to check null and empty auction creation fields
-function checkNullCreateFields (...args) {
-  let createFields = ["account","tokenID","contractAddress","startTime","endTime","minBid","auctionType"];
-  for(let i = 0;i < args.length; i++){
-    if (args[i] == null || (!/\S/.test(args[i]))) {
-      return {data: createFields[i], value:false};
+function checkNullCreateFields(...args) {
+  let createFields = [
+    "account",
+    "tokenID",
+    "contractAddress",
+    "startTime",
+    "endTime",
+    "minBid",
+    "auctionType",
+  ];
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] == null || !/\S/.test(args[i])) {
+      return { data: createFields[i], value: false };
     }
   }
-  return{data:null, value:true};
+  return { data: null, value: true };
 }
 
 // Function to check null and empty bid request fields
-function checkNullBidFields (...args) {
-  let bidFields = ["account","bidAmt","bidMsg"];
-  for(let i = 0;i < args.length; i++){
-    if (args[i] == null || (!/\S/.test(args[i]))) {
-      return {data: bidFields[i], value:false};
+function checkNullBidFields(...args) {
+  let bidFields = ["account", "bidAmt", "bidMsg"];
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] == null || !/\S/.test(args[i])) {
+      return { data: bidFields[i], value: false };
     }
   }
-  return{data:null, value:true};
+  return { data: null, value: true };
 }
 
 // Function to get current time in seconds, for logging purposes
@@ -55,30 +63,36 @@ function curTimeInSec() {
 }
 
 // Returns a list of auctions
-router.get('/auctions', async (req, res, next) => {
-  var t = curTimeInSec();
+router.get("/auctions", async (req, res, next) => {
+  let t = curTimeInSec();
   try {
-    console.log(t,"GET request to /auctions from", req.ip)
+    console.log(t, "GET request to /auctions from", req.ip);
     // pull list of files in bucket from fleek
-    await fleek.listFiles({
-      apiKey: secrets.apiKey,
-      apiSecret: secrets.apiSecret,
-      getOptions: [
-        'key'
-      ],
-    }).then((file) => res.send(file));
+    await fleek
+      .listFiles({
+        apiKey: secrets.apiKey,
+        apiSecret: secrets.apiSecret,
+        getOptions: ["key"],
+      })
+      .then((file) => res.send(file));
   } catch (error) {
     next(error);
   }
 });
 
 // Creates a new auction
-router.post('/auction', async (req, res, next) => {
-  var t = curTimeInSec();
-  console.log(t,"POST request to /auction from", req.ip, "with body params:",req.body)
+router.post("/auction", async (req, res, next) => {
+  let t = curTimeInSec();
+  console.log(
+    t,
+    "POST request to /auction from",
+    req.ip,
+    "with body params:",
+    req.body
+  );
   try {
     // check null and empty request fields
-    var result = checkNullCreateFields(
+    let result = checkNullCreateFields(
       req.body.account,
       req.body.tokenID,
       req.body.contractAddress,
@@ -87,8 +101,8 @@ router.post('/auction', async (req, res, next) => {
       req.body.minBid,
       req.body.auctionType
     );
-    if (result['value'] == false) {
-        return res.status(400).send({"message": result['data'] + " not found"});
+    if (result["value"] == false) {
+      return res.status(400).send({ message: result["data"] + " not found" });
     }
 
     // is this correct?
@@ -108,125 +122,163 @@ router.post('/auction', async (req, res, next) => {
       minBid: req.body.minBid,
       auctionType: req.body.auctionType,
       currentBidder: "",
-      currentBid: 0
+      currentBid: 0,
     };
     // upload to fleek
-    await fleek.upload({
-      apiKey: secrets.apiKey,
-      apiSecret: secrets.apiSecret,
-      key: t + req.body.account,
-      data: JSON.stringify(data)
-    }).then(() => res.status(200).send({"message":"Ok"}));
+    await fleek
+      .upload({
+        apiKey: secrets.apiKey,
+        apiSecret: secrets.apiSecret,
+        key: t + req.body.account,
+        data: JSON.stringify(data),
+      })
+      .then(() => res.status(200).send({ message: "Ok" }));
   } catch (error) {
     next(error);
   }
 });
 
 // Creates a new bid for an auction
-router.post('/auctions/:auctionID/bid', async (req, res, next) => {
-  var t = curTimeInSec();
-  console.log(t,"POST request to /auctions/", req.params.auctionID, "/bid from", req.ip, "with body params:",req.body)
+router.post("/auctions/:auctionID/bid", async (req, res, next) => {
+  let t = curTimeInSec();
+  console.log(
+    t,
+    "POST request to /auctions/",
+    req.params.auctionID,
+    "/bid from",
+    req.ip,
+    "with body params:",
+    req.body
+  );
   try {
-    var result = checkNullBidFields(
+    let result = checkNullBidFields(
       req.body.account, // account of the bidder
       req.body.bidAmt,
       req.body.bidMsg // signed msg
     );
-    if (result['value'] == false) {
-        return res.status(400).send({"message": result['data'] + " not found"});
+    if (result["value"] == false) {
+      return res.status(400).send({ message: result["data"] + " not found" });
     }
     // pull auction from fleek
-	  await fleek.get({
-      apiKey: secrets.apiKey,
-      apiSecret: secrets.apiSecret,
-      key: req.params.auctionID
-    }).then(async (auction) => {
-      // then parse data & add currentBidder and currentBid
-      if (auction.data) {
-        var oldAuction = JSON.parse(auction.data);
-        const data = {
-          account: oldAuction.account,
-          tokenID: oldAuction.tokenID,
-          contractAddress: oldAuction.contractAddress,
-          startTime: oldAuction.startTime,
-          endTime: oldAuction.endTime,
-          minBid: oldAuction.minBid,
-          auctionType: oldAuction.auctionType,
-          currentBidder: req.body.account,
-          currentBid: req.body.bidAmt,
-          bidMsg: req.body.bidMsg
-        };
-        // delete the old auction
-        await fleek.deleteFile({
-          apiKey: secrets.apiKey,
-          apiSecret: secrets.apiSecret,
-          key: req.params.auctionID
-        }).then(async () => {
-          // and upload new auction under the same name (key)
-          await fleek.upload({
-            apiKey: secrets.apiKey,
-            apiSecret: secrets.apiSecret,
-            key: req.params.auctionID,
-            data: JSON.stringify(data)
-          }).then(() => res.status(200).send({"message":"Ok"}));
-        });
-      } else {
-        return res.status(500).send({"message":"An error occurred when posting bid"});
-      }
-
-    });
-
+    await fleek
+      .get({
+        apiKey: secrets.apiKey,
+        apiSecret: secrets.apiSecret,
+        key: req.params.auctionID,
+      })
+      .then(async (auction) => {
+        // then parse data & add currentBidder and currentBid
+        if (auction.data) {
+          let oldAuction = JSON.parse(auction.data);
+          const data = {
+            account: oldAuction.account,
+            tokenID: oldAuction.tokenID,
+            contractAddress: oldAuction.contractAddress,
+            startTime: oldAuction.startTime,
+            endTime: oldAuction.endTime,
+            minBid: oldAuction.minBid,
+            auctionType: oldAuction.auctionType,
+            currentBidder: req.body.account,
+            currentBid: req.body.bidAmt,
+            bidMsg: req.body.bidMsg,
+          };
+          // delete the old auction
+          await fleek
+            .deleteFile({
+              apiKey: secrets.apiKey,
+              apiSecret: secrets.apiSecret,
+              key: req.params.auctionID,
+            })
+            .then(async () => {
+              // and upload new auction under the same name (key)
+              await fleek
+                .upload({
+                  apiKey: secrets.apiKey,
+                  apiSecret: secrets.apiSecret,
+                  key: req.params.auctionID,
+                  data: JSON.stringify(data),
+                })
+                .then(() => res.status(200).send({ message: "Ok" }));
+            });
+        } else {
+          return res
+            .status(500)
+            .send({ message: "An error occurred when posting bid" });
+        }
+      });
   } catch (error) {
     next(error);
   }
 });
 
 // Endpoint returns a specific auction, given an auction id
-router.get('/auctions/:auctionID', limiter, async (req, res, next) => {
-  var t = curTimeInSec();
+router.get("/auctions/:auctionID", limiter, async (req, res, next) => {
+  let t = curTimeInSec();
   try {
-    console.log(t,"GET request to /auctions/", req.params.auctionID, "from", req.ip)
-    if (req.params.auctionID == null || (!/\S/.test(req.params.auctionID))) {
-      return res.send({"status":"false","message":"Please provide an auction id"});
+    console.log(
+      t,
+      "GET request to /auctions/",
+      req.params.auctionID,
+      "from",
+      req.ip
+    );
+    if (req.params.auctionID == null || !/\S/.test(req.params.auctionID)) {
+      return res.send({
+        status: "false",
+        message: "Please provide an auction id",
+      });
     }
     // get file with key from fleek
-	  await fleek.get({
-      apiKey: secrets.apiKey,
-      apiSecret: secrets.apiSecret,
-      key: req.params.auctionID
-    }).then((file) => {
-      //console.log(JSON.parse(file.data))
-      res.send(JSON.parse(file.data));
-    });
+    await fleek
+      .get({
+        apiKey: secrets.apiKey,
+        apiSecret: secrets.apiSecret,
+        key: req.params.auctionID,
+      })
+      .then((file) => {
+        //console.log(JSON.parse(file.data))
+        res.send(JSON.parse(file.data));
+      });
   } catch (error) {
     next(error);
   }
 });
 
 // Endpoint to return bids, given an auction id
-router.get('/auctions/:auctionID/bids', limiter, async (req, res, next) => {
-  var t = curTimeInSec();
+router.get("/auctions/:auctionID/bids", limiter, async (req, res, next) => {
+  let t = curTimeInSec();
   try {
-    console.log(t,"GET request to /auctions/", req.params.auctionID,"/bids from", req.ip)
-    if (req.params.auctionID == null || (!/\S/.test(req.params.auctionID))) {
-      return res.send({"status":"false","message":"Please provide an auction id"});
+    console.log(
+      t,
+      "GET request to /auctions/",
+      req.params.auctionID,
+      "/bids from",
+      req.ip
+    );
+    if (req.params.auctionID == null || !/\S/.test(req.params.auctionID)) {
+      return res.send({
+        status: "false",
+        message: "Please provide an auction id",
+      });
     }
     // get file with key from fleek
-	  await fleek.get({
-      apiKey: secrets.apiKey,
-      apiSecret: secrets.apiSecret,
-      key: req.params.auctionID
-    }).then((file) => {
-      // parse file and return only bids
-      auction = JSON.parse(file.data);
-      console.log(auction)
-      bids = {
-        currentBidder: auction.currentBidder,
-        currentBid: auction.currentBid,
-        currentBidder: auction.currentBidder,
-      };
-      res.json(bids);
-    });
+    await fleek
+      .get({
+        apiKey: secrets.apiKey,
+        apiSecret: secrets.apiSecret,
+        key: req.params.auctionID,
+      })
+      .then((file) => {
+        // parse file and return only bids
+        auction = JSON.parse(file.data);
+        console.log(auction);
+        bids = {
+          currentBidder: auction.currentBidder,
+          currentBid: auction.currentBid,
+          currentBidder: auction.currentBidder,
+        };
+        res.json(bids);
+      });
   } catch (error) {
     next(error);
   }
