1ecebc2c403a627a208381724da587dd11aaf955
James Earle <j_earle@hotmail.com>
2021-08-30 14:54:45 -0700

    add rudimentary test for circle

---
+++ test/api.test.ts
@@ -0,0 +1,11 @@
+import { expect } from "chai";
+import router from "../src/api";
+
+describe("API Tests", () => {
+  describe("/bid", () => {
+    it("works", () => {
+      console.log("success");
+      expect(1 == 1);
+    });
+  });
+});
