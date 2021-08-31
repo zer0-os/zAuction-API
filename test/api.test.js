15abc5c72bb8411d096d0b59b22748f0d0e7d3fa
James Earle <j_earle@hotmail.com>
2021-08-30 17:02:27 -0700

    add a few basic tests

--- test/api.test.js
+++ test/api.test.js
@@ -1,23 +1,46 @@
 const request = require("supertest");
-
 const app = require("../src/app");
 
-describe("GET /api/fleek/listFiles", () => {
-  it("responds with a json message", (done) => {
-    request(app)
-      .get("/api/fleek/listFiles")
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200, done);
-  });
-});
+describe("API Endpoint Tests", () => {
 
-describe("GET /api/emojis", () => {
-  it("responds with a json message", (done) => {
-    request(app)
-      .get("/api/v1/emojis")
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200, ["😀", "😳", "🙄"], done);
+  describe("/bid Tests", () => {
+    it("Fails on invalid input", () => {
+      const badPayload = {
+        foo: "bar"
+      }
+      
+      request(app)
+        .post("/bid")
+        .send(badPayload)
+        .expect(400)
+    }),
+    // mock storage with sinon, don't touch fleek
+    it("Returns 200 on expected input", () => {
+      const payload = {
+        nftIds: [
+          "0x1234",
+          "0x5678"
+        ]
+      }
+
+      request(app)
+        .post("/bid")
+        .send(payload)
+        .expect(200)
+        // expect the bids object not just status?
+    })
   });
+
+  describe("/bids/list Tests", () => {
+    it("Fails on invalid input", () => {
+      const badPayload = {
+        foo: "bar"
+      }
+      
+      request(app)
+        .post("/bids/list")
+        .send(badPayload)
+        .expect(400)
+    })
+  })
 });
