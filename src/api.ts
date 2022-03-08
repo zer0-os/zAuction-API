85b55cb3bb6e4d7433cd2d8067222b457cbcfc13
James Earle <j_earle@hotmail.com>
2022-03-07 16:23:34 -0800

    improve the way the connection to eventhub is managed

--- src/api.ts
+++ src/api.ts
@@ -231,7 +231,7 @@ router.post(
       };
 
       // Add new bid to our event queue
-      //await queue.sendMessage(message);
+      await queue.sendMessage(message);
 
       return res.status(200).send("OK");
     } catch (error) {
@@ -304,22 +304,23 @@ router.post(
       // Once confirmed, move to archive collection
       await database.cancelBid(bidData, archiveCollection);
 
-      // const message: TypedMessage<BidCancelledV1Data> = {
-      //   event: MessageType.BidCancelled,
-      //   version: "1.0",
-      //   timestamp: new Date().getTime(),
-      //   logIndex: undefined,
-      //   blockNumber: undefined,
-      //   data: {
-      //     account: signer,
-      //     uniqueBidId: bidData.uniqueBidId,
-      //   },
-      // };
+      const message: TypedMessage<BidCancelledV1Data> = {
+        event: MessageType.BidCancelled,
+        version: "1.0",
+        timestamp: new Date().getTime(),
+        logIndex: undefined,
+        blockNumber: undefined,
+        data: {
+          account: signer,
+          uniqueBidId: bidData.uniqueBidId,
+        },
+      };
 
-      // await queue.sendMessage(message);
+      await queue.sendMessage(message);
+      // await queue.close(); 
 
       return res.status(200).send();
-    } catch (e) {
+    } catch (e: any) { // have to type message?
       console.error(e.message, e.stack);
       next(
         new Error(
--- src/messagequeue/adapters/eventhub.ts
+++ src/messagequeue/adapters/eventhub.ts
@@ -1,13 +1,16 @@
 import { EventData, EventHubProducerClient } from "@azure/event-hubs";
 import { Message } from "@zero-tech/zns-message-schemas";
+import { MessageQueueService } from "..";
 
-export const create = (connectionString: string, name: string) => {
-  const producer: EventHubProducerClient = new EventHubProducerClient(
-    connectionString,
-    name
-  );
-
+export const create = (
+  connectionString: string,
+  name: string
+): MessageQueueService => {
   const sendMessage = async (message: Message) => {
+    const producer: EventHubProducerClient = new EventHubProducerClient(
+      connectionString,
+      name
+    );
     const batch = await producer.createBatch();
 
     const event: EventData = {
@@ -20,7 +23,7 @@ export const create = (connectionString: string, name: string) => {
     await producer.close();
   };
 
-  const messageQueueService = {
+  const messageQueueService: MessageQueueService = {
     sendMessage,
   };
   return messageQueueService;
